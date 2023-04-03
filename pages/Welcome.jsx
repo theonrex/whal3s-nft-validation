6730d1c74e1a2bf2aa04e283f62bb3b027d1908a
theonrex <theonrex1@gmail.com>
2023-04-03 01:21:16 +0100

    welcome page fix

--- pages/Welcome.jsx
+++ pages/Welcome.jsx
@@ -5,10 +5,7 @@ import NftValidationUtilitySection from "../components/Utilities/NftValidationUt
 export default function WelcomePage() {
   return (
     <>
-      <div
-       
-        className="container mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-12 sm:py-18 lg:py-24 font-sans"
-      >
+      <div className="container mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-12 sm:py-18 lg:py-24 font-sans">
         <NftValidationUtilitySection />
       </div>
       <Toaster />
