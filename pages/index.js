ecd4d590b7d233a8b01e814df621ec68b360cf1b
Jonas Esser <jonas@whal3s.xyz>
2023-04-03 19:56:19 +0000

    Cleanup by Whal3s

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
 import React, { useEffect, useState } from "react";
-import dynamic from "next/dynamic";
-
-const NftValidationUtilitySection = dynamic(
-  () => import("../Utilities/NftValidationUtilitySection"),
-  { ssr: false }
-);
 import WelcomePage from "../components/WelcomePage";
 import { Toaster } from "react-hot-toast";
+import DynamicNftValidationUtilitySection from "../Utilities/DynamicNftValidationUtilitySection";
+
+
 
 export default function Home() {
   
@@ -18,7 +15,7 @@ export default function Home() {
         id="claim"
         className="container mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-12 sm:py-18 lg:py-24 font-sans"
       >
-        <NftValidationUtilitySection />
+        <DynamicNftValidationUtilitySection />
       </div>
       <Toaster />
     </>
